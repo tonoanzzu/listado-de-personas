3a5a07de7ed5970f7303a8e7792439910028959e
Antonio Anzures <joaz@gft.com>
2020-01-30 12:53:30 -0600

    Listado de personas usando ngFor de Angular

---
+++ src/app/persona.model.ts
@@ -0,0 +1,13 @@
+export class Persona{
+  //sintaxis simplificada
+  /* constructor(nombre:string, apellido:string) */
+  nombre: string;
+  apellido: string;
+
+  constructor(nombre:string, apellido:string){
+    this.nombre = nombre;
+    this.apellido = apellido;
+  }
+
+   
+}
